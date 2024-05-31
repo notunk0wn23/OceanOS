9061bb9b505107f985d4f491671d9daa5cd4fb16
spaced <asherlr2@gmail.com>
2024-05-31 10:27:19 -0500

    Rewrite filesystem class AGAIN (hopefully wont need to again)

--- src/modules/FileSystem.js
+++ src/modules/FileSystem.js
@@ -1,48 +1,103 @@
-export class FileSystem {
-  constructor() {
-    this.root = new Directory("/");
-  }
-
-  // Get a specific file or directory by path
-  get(path) {
-    const parts = path.split("/"); // Split path by "/" separator
-    let current = this.root;
-    for (let i = 1; i < parts.length; i++) {
-      const name = parts[i];
-      current = current.get(name);
-      if (!current) {
-        return null; // Path not found
-      }
+class File {
+    constructor(name, content = '') {
+        this.name = name;
+        this.content = content;
     }
-    return current;
-  }
 }
 
 class Directory {
-  constructor(name) {
-    this.name = name;
-    this.children = {}; // Key: child name, Value: child object (File or Directory)
-  }
-
-  // Add a file or directory to the current directory
-  add(child) {
-    if (child.name in this.children) {
-      throw new Error("File or directory already exists");
+    constructor(name) {
+        this.name = name;
+        this.children = {};
     }
-    this.children[child.name] = child;
-  }
 
-  // Get a file or directory by name from the current directory
-  get(name) {
-    return this.children[name];
-  }
+    addFile(file) {
+        if (this.children[file.name]) {
+            throw new Error(`File or directory named ${file.name} already exists.`);
+        }
+        this.children[file.name] = file;
+    }
+
+    addDirectory(directory) {
+        if (this.children[directory.name]) {
+            throw new Error(`File or directory named ${directory.name} already exists.`);
+        }
+        this.children[directory.name] = directory;
+    }
+
+    getFile(fileName) {
+        if (!this.children[fileName] || this.children[fileName] instanceof Directory) {
+            throw new Error(`File named ${fileName} does not exist.`);
+        }
+        return this.children[fileName];
+    }
+
+    getDirectory(directoryName) {
+        if (!this.children[directoryName] || this.children[directoryName] instanceof File) {
+            throw new Error(`Directory named ${directoryName} does not exist.`);
+        }
+        return this.children[directoryName];
+    }
 }
 
-class File {
-  constructor(name, content) {
-    this.name = name;
-    this.content = content;
-  }
+export class FileSystem {
+    constructor() {
+        this.root = new Directory('/');
+    }
+
+    createFile(path, content = '') {
+        const parts = path.split('/').filter(part => part);
+        const fileName = parts.pop();
+        let currentDirectory = this.root;
+
+        for (const part of parts) {
+            if (!currentDirectory.children[part]) {
+                currentDirectory.addDirectory(new Directory(part));
+            }
+            currentDirectory = currentDirectory.getDirectory(part);
+        }
+
+        currentDirectory.addFile(new File(fileName, content));
+    }
+
+    readFile(path) {
+        const parts = path.split('/').filter(part => part);
+        const fileName = parts.pop();
+        let currentDirectory = this.root;
+
+        for (const part of parts) {
+            currentDirectory = currentDirectory.getDirectory(part);
+        }
+
+        return currentDirectory.getFile(fileName).content;
+    }
+
+    writeFile(path, content) {
+        const parts = path.split('/').filter(part => part);
+        const fileName = parts.pop();
+        let currentDirectory = this.root;
+
+        for (const part of parts) {
+            currentDirectory = currentDirectory.getDirectory(part);
+        }
+
+        const file = currentDirectory.getFile(fileName);
+        file.content = content;
+    }
+
+    deleteFile(path) {
+        const parts = path.split('/').filter(part => part);
+        const fileName = parts.pop();
+        let currentDirectory = this.root;
+
+        for (const part of parts) {
+            currentDirectory = currentDirectory.getDirectory(part);
+        }
+
+        if (!currentDirectory.children[fileName]) {
+            throw new Error(`File named ${fileName} does not exist.`);
+        }
+
+        delete currentDirectory.children[fileName];
+    }
 }
-
-
