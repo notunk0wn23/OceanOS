ad47f14424111faee953e3c1e722a4df42bbc94f
spaced <asherlr2@gmail.com>
2024-05-31 11:47:11 -0500

    Support styling for the window directly

--- src/modules/DisplayManager.js
+++ src/modules/DisplayManager.js
@@ -9,70 +9,81 @@ import { UIElement } from "./UIElement.js";
  * */
 
 const app = {
-    width: 100,
-    height: 100,
-    title: "Example App",
-    children: [
-        {
-            type: "button",
-            text: "Hello World",
-            geometry: {
-                x: 0,
-                y: 0,
-                width: 50,
-                height: 100,
-            },
-            onClick: function() {
-                console.log("Hello world button pressed");
-            },
-        },
-    ],
+  width: 100,
+  height: 100,
+  title: "Example App",
+  children: [
+    {
+      type: "button",
+      text: "Hello World",
+      geometry: {
+        x: 0,
+        y: 0,
+        width: 50,
+        height: 100,
+      },
+      onClick: function () {
+        console.log("Hello world button pressed");
+      },
+    },
+  ],
 };
 
 export class DisplayManager {
-    constructor() {
-        this.windows = [];
+  constructor() {
+    this.windows = [];
+  }
+
+  initializeWindow(window) {
+    let localWindow = window;
+    let windowElement = document.createElement("div");
+    localWindow.element = windowElement;
+
+    localWindow.element.style.overflow = "hidden";
+    localWindow.element.style.width = window.width + "px";
+    localWindow.element.style.height = window.height + "px";
+
+    if (localWindow.style) {
+      Object.assign(windowElement.style, localWindow.style);
     }
 
-    initializeWindow(window) {
-        let localWindow = window;
-        let windowElement = document.createElement("div");
-        localWindow.element = windowElement;
+    localWindow.children.forEach((component) =>
+      this.renderComponent(component, localWindow.element),
+    );
 
-        localWindow.element.style.overflow = "hidden";
-        localWindow.element.style.width = window.width + "px";
-        localWindow.element.style.height = window.height + "px";
+    this.windows.push(localWindow);
+    document.body.appendChild(localWindow.element);
+  }
 
-        localWindow.children.forEach((component) =>
-            this.renderComponent(component, localWindow.element),
-        );
-
-        this.windows.push(localWindow);
-        document.body.appendChild(localWindow.element);
+  renderComponent(component, container) {
+    const element = document.createElement(component.type);
+    if (component.text) {
+      element.textContent = component.text;
+    }
+    element.style.position = "relative"; // Ensure absolute positioning for geometry
+    if (component.geometry) {
+      if (typeof component.geometry.x == "number") {
+        element.style.left = component.geometry.x + "px";
+      } else {
+      }
+      element.style.top = component.geometry.y + "px";
+      element.style.width = component.geometry.width + "px";
+      element.style.height = component.geometry.height + "px";
+    }
+    if (component.style) {
+      Object.assign(element.style, component.style);
     }
 
-    renderComponent(component, container) {
-        const element = document.createElement(component.type);
-        if (component.text) {
-            element.textContent = component.text;
-        }
-        element.style.position = "relative"; // Ensure absolute positioning for geometry
-        if (component.geometry) {
-            element.style.left = component.geometry.x + "px";
-            element.style.top = component.geometry.y + "px";
-            element.style.width = component.geometry.width + "px";
-            element.style.height = component.geometry.height + "px";
-        }
-        if (component.style) {
-            Object.assign(element.style, component.style);
-        }
-
-        if (component.onClick) {
-            element.addEventListener("click", component.onClick)
-        }
-        if (component.children) {
-            component.children.forEach((child) => this.renderComponent(child));
-        }
-        container.appendChild(element);
+    if (component.onClick) {
+      element.addEventListener("click", component.onClick);
     }
+    if (component.children) {
+      component.children.forEach((child) => this.renderComponent(child));
+    }
+
+    container.appendChild(element);
+    if (component.onLoad) {
+      component.onLoad();
+    }
+  }
 }
